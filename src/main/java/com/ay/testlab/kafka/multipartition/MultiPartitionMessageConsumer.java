fccc5284e1b2c2148b185017e5e4fb7f743ecfb6
ayortanli <ahmetyortanli@gmail.com>
2018-04-13 11:05:41 +0300

    Multi-partition based 1 producer - 6 consumer messaging is implemented

--- src/main/java/com/ay/testlab/kafka/multipartition/MultiPartitionMessageConsumer.java
+++ src/main/java/com/ay/testlab/kafka/multipartition/MultiPartitionMessageConsumer.java
@@ -1,6 +1,7 @@
 package com.ay.testlab.kafka.multipartition;
 
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,12 +13,10 @@ public class MultiPartitionMessageConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MultiPartitionMessageConsumer.class);
 
-    //https://memorynotfound.com/spring-kafka-batch-listener-example/
-    //https://docs.spring.io/spring-kafka/api/org/springframework/kafka/support/KafkaHeaders.html#CONSUMER
-    //Headerda aşağıdaki bilgileri de yazdıralım...
-
     @KafkaListener(topics = "${kafka.topic.multiPartitionTopic}", containerFactory = "KafkaListenerContainerFactoryWith6Consumer", groupId = "multiPartitionWith6Consumer")
-    public void receive(@Payload String payload, @Header(KafkaHeaders.CONSUMER)Consumer<String,String> consumer) {
-        LOGGER.info("Received payload='{}' from topic-partition@offset:consumerId='{}'", payload, consumer);
+    public void receive(@Payload String payload,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID)Long partitionId,
+                        @Header(KafkaHeaders.OFFSET)Long offset) {
+        LOGGER.info("Received payload='{}' from partitionId@offset='{}'", payload, partitionId+"@"+offset);
     }
 }
